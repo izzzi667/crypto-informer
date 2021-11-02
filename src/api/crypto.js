a931d08d22450f2a0cd2d4c9fed615346cffc858
Aleksey Evdokimov <izzzi667@gmail.com>
2021-11-02 20:54:01 +0300

    Refactoring api file

--- src/api/crypto.js
+++ src/api/crypto.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 
 const instancse = axios.create({
     baseURL: 'https://api.coingecko.com/api/v3/'
@@ -9,15 +9,15 @@ export const cryptoApi =
 {
     getCoinsDetailedList (currency='usd',per_page=10, page=1, order='market_cap_desc')
     {
-        return instancse.get('coins/markets?vs_currency='+currency+'&order='+order+'&per_page='+per_page+'&page='+page+'&sparkline=false&price_change_percentage=1h,24h,7d,14d,30d,200d,1y')
+        return instancse.get(`coins/markets?vs_currency=${currency}&order=${order}&per_page=${per_page}&page=${page}&sparkline=false&price_change_percentage=1h,24h,7d,14d,30d,200d,1y`)
     },
     getCoinInformation (coinId)
     {
-        return  instancse.get('coins/'+coinId);        
+        return  instancse.get(`coins/${coinId}`);
     },
     getCoinHistoryData (coinId, VScurrency = 'usd', days=30)
     {      
-        return instancse.get ('coins/'+coinId+'/market_chart?vs_currency='+VScurrency+'&days='+days);
+        return instancse.get (`coins/${coinId}/market_chart?vs_currency=${VScurrency}&days=${days}`);
     },
     getGlobalData ()
     {
@@ -52,7 +52,6 @@ export const exchangeApi =
     {
         return instancse.get(`exchanges/${id}/volume_chart?days=${numberOfDays}`);
     }
-
 }
 
 export default cryptoApi;
