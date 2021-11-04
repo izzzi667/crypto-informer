e267b79d8311d36c8026757d939e6d59eb9ba08e
izzzi667 <izzzi667@gmail.com>
2021-11-04 21:37:16 +0300

    Refactoring Coins Card

--- src/components/Coins/CoinsListCard/CoinsListCard.jsx
+++ src/components/Coins/CoinsListCard/CoinsListCard.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Card, ListGroup, Col } from "react-bootstrap";
 import DateDiff from "../../Common/DateDIff";
-import { Icon } from "@fluentui/react";
 import { NavLink } from "react-router-dom";
+import ThrendingIcon from "../../Common/ThrendingIcon";
 
 
 const CoinsListCard = (props) =>
@@ -16,8 +16,8 @@ const CoinsListCard = (props) =>
                     <Card.Text><small>Market cap rank: {props.coinInfo.market_cap_rank}</small></Card.Text>
                 </Card.Body>
                 <ListGroup variant="flush">
-                    <ListGroup.Item variant={varianValue}>Price: {props.coinInfo.current_price}$ {props.coinInfo.price_change_24h>0?<Icon iconName="TriangleSolidUp12" />:<Icon iconName="TriangleSolidDown12" />}</ListGroup.Item>
-                    <ListGroup.Item variant={varianValue}>Price change 24h:<br /> {props.coinInfo.price_change_24h} </ListGroup.Item>
+                    <ListGroup.Item variant={varianValue}>Price: {props.coinInfo.current_price}$ </ListGroup.Item>
+                    <ListGroup.Item variant={varianValue}>Price change 24h:<br /> {props.coinInfo.price_change_24h}<ThrendingIcon value={props.coinInfo.price_change_24h}/> </ListGroup.Item>
                     <ListGroup.Item variant={varianValue}>Low/High 24h: <br />{props.coinInfo.low_24h}$/{props.coinInfo.high_24h}$</ListGroup.Item>
                 </ListGroup>
                 <Card.Footer>
