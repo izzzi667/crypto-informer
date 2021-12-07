bfe887ee7e8e25b6d7314ab0bf5bfccc6e682de4
Aleksey Evdokimov <izzzi667@gmail.com>
2021-12-07 20:11:22 +0300

    Update loading component

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
-import { ProgressIndicator } from '@fluentui/react/lib/ProgressIndicator';
+import { Spinner, Col, Row} from "react-bootstrap";
+
 
 const Loading = () =>
 {
-    return <ProgressIndicator label="Loading"  />
+    return <Col className='text-center' >
+        <Spinner animation="border" variant="primary" />
+        <Spinner animation="border" variant="success" />
+        <Spinner animation="border" variant="danger" />
+        <Spinner animation="border" variant="warning" />
+        <Spinner animation="border" variant="info" />
+    </Col>
 }
 
 export default Loading;
